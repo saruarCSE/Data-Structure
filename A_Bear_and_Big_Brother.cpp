921b9d930d2de37a46388996a16e8309c7b5d9a1
Saruar CSE <saruarhossain778620@gmail.com>
2025-05-12 22:00:30 +0600

    Add files via upload

---
+++ A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int a,b;
+    cin>>a>>b;
+
+    int m=0;
+
+    while (a<=b)
+    {
+        a *= 3;
+        b *= 2;
+
+        m++;
+    }
+
+    cout<<m<<"\n";
+    
+}
