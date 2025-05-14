99f3d1bae5056325737885bcc649663630303e10
Saruar CSE <saruarhossain778620@gmail.com>
2025-05-15 02:49:36 +0600

    Add files via upload

---
+++ module_6/insert_at_tail.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head,int val){
+    Node* newnode = new Node(val);
+    head->next->next->next = newnode;
+    newnode = NULL;
+}
+
+void print_link_list(Node* head){
+    while (head != NULL)
+    {
+        cout<<head->val<<"\n";
+        head = head->next;
+    }
+    
+}
+
+int main(){
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b  = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    insert_at_tail(head,100);
+    print_link_list(head);
+}
